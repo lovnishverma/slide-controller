33497127637c55eccb4d289456821d25a2f8c9ff
Lovnish Verma <princelv84@gmail.com>
2025-05-03 22:34:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+# mqtt_slide_control.py
+import paho.mqtt.client as mqtt
+import pyautogui
+import ssl
+
+# MQTT Broker Configuration
+BROKER = "95bd7f2a55ad4dd799618a95837e4303.s1.eu.hivemq.cloud"
+PORT = 8883
+USERNAME = "lovnish"
+PASSWORD = "Nielit@123"
+TOPIC = "presentation/control"
+
+# MQTT Callback: On successful connection
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code", rc)
+    client.subscribe(TOPIC)
+
+# MQTT Callback: On message received
+
+
+def on_message(client, userdata, msg):
+    command = msg.payload.decode().strip().lower()
+    print("Command received:", command)
+
+    if command == "next":
+        pyautogui.press("right")
+    elif command == "prev":
+        pyautogui.press("left")
+    elif command == "start":
+        pyautogui.press("f5")
+    elif command == "exit":
+        pyautogui.press("esc")
+    elif command == "current":
+        pyautogui.hotkey("shift", "f5")
+    else:
+        print("Unknown command")
+
+
+# MQTT Client Setup
+client = mqtt.Client()
+client.username_pw_set(USERNAME, PASSWORD)
+client.tls_set(cert_reqs=ssl.CERT_REQUIRED)
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Connect to MQTT Broker and Start Listening
+client.connect(BROKER, PORT)
+client.loop_forever()
